34ecd5f45247ba9f18a444715e2e90d3104f16af
PascualHache <hector.pascual.diaz@gmail.com>
2020-02-25 13:20:43 +0100

    Preparando fase 2

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,11 @@
 "use strict";
+window.onload = iniciar;
 var id = 0;
 var rocketsObj = {};
+function iniciar() {
+    var listenerr = document.getElementById("login");
+    listenerr.addEventListener('guardaRocket', validateForm, false);
+}
 function createRocket(code, thrusters) {
     rocketsObj[code] = new newRocket(code, thrusters);
     var rocEle = document.getElementById("rocketsList");
@@ -8,6 +13,7 @@ function createRocket(code, thrusters) {
     rocEle.innerText = rocEleContent + " " + code + " " + thrusters + "<br />";
 }
 function toggle() {
+    clearFields();
     var t1 = document.getElementById("tab1");
     var t2 = document.getElementById("tab2");
     if (t2.style.display === "inline-block") {
@@ -22,11 +28,90 @@ function toggle() {
 function introduceRocket() {
     var codeName = document.getElementById("code");
     var thrusters = document.getElementById("thrusters");
+    var potencia = document.getElementById("potencia");
     createRocket(codeName.value, thrusters.value);
     toggle();
     console.log(rocketsObj);
 }
+function validateForm() {
+    if (validaCode() && validaThrusters() && validaPropulsores()) {
+        introduceRocket();
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function validaPropulsores() {
+    var potencia = document.getElementById("potencia");
+    var numeroPropul = document.getElementById("thrusters");
+    var arr = potencia.value.split(",").map(Number);
+    var arrPrecessed = validaArraydeNums(arr);
+    console.log("Validacion de propulsores");
+    console.log(arrPrecessed.length);
+    console.log(arrPrecessed.length != parseInt(numeroPropul.value));
+    console.log(potencia.validity.valueMissing);
+    console.log("--------");
+    if (!potencia.checkValidity()) {
+        console.log("loco");
+        if (potencia.validity.valueMissing) {
+            alert("Introduce un valor numérico por número de propulsores separado por comas");
+        }
+        else if (arrPrecessed.length != parseInt(numeroPropul.value)) {
+            console.log("AAAAAA");
+            alert("El número de propulsores no se corresponde con las potencias introducidas");
+        }
+        return false;
+    }
+    return true;
+}
+function validaArraydeNums(arrPotencia) {
+    var arrTemp = arrPotencia;
+    for (var index = 0; index < arrPotencia.length; index++) {
+        if (isNaN(arrPotencia[index])) {
+            arrTemp.splice(index, 1);
+        }
+    }
+    // console.log(arrTemp);
+    return arrTemp;
+}
+function validaCode() {
+    var elemento = document.getElementById("code");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            alert("Introduce un valor de 8 dígitos en el código del cohete");
+        }
+        else if (elemento.validity.patternMismatch) {
+            alert("Estas seguro que tiene 8 dígitos?");
+        }
+        return false;
+    }
+    return true;
+}
+function validaThrusters() {
+    var elemento = document.getElementById("thrusters");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            alert("Introduce un valor numérico en los propulsores");
+        }
+        else if (elemento.validity.patternMismatch) {
+            alert("Estas seguro que es un valor numérico?");
+        }
+        return false;
+    }
+    return true;
+}
 function myButtonClicked(el) {
     el.disabled = true;
     toggle();
+}
+function clearFields() {
+    var codeName = document.getElementById("code");
+    var thrusters = document.getElementById("thrusters");
+    var potencia = document.getElementById("potencia");
+    if (codeName.value != "" || thrusters.value != "" || potencia.value != "") {
+        codeName.value = "";
+        thrusters.value = "";
+        potencia.value = "";
+    }
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,19 +1,26 @@
+window.onload = iniciar;
+
 var id = 0;
 var rocketsObj: any = {};
+
+function iniciar() {
+    let listenerr: any = document.getElementById("login");
+    listenerr.addEventListener('guardaRocket', validateForm, false);
+}
 
 function createRocket(code: string, thrusters: number) {
     rocketsObj[code] = new newRocket(code, thrusters);
     let rocEle: any = document.getElementById("rocketsList");
     let rocEleContent = rocEle.innerHTML;
-    rocEle.innerText= rocEleContent+" "+code+" "+ thrusters+"<br />";
+    rocEle.innerText = rocEleContent + " " + code + " " + thrusters + "<br />";
 }
 
 function toggle() {
-
+    clearFields();
     let t1: any = document.getElementById("tab1");
     let t2: any = document.getElementById("tab2");
-    
-    
+
+
     if (t2.style.display === "inline-block") {
         t2.style.display = "none";
         t1.style.display = "inline-block";
@@ -25,16 +32,101 @@ function toggle() {
 
 }
 
-function introduceRocket(){
+function introduceRocket() {
     let codeName: any = document.getElementById("code");
     let thrusters: any = document.getElementById("thrusters");
+    let potencia: any = document.getElementById("potencia");
     createRocket(codeName.value, thrusters.value);
     toggle();
     console.log(rocketsObj);
 }
 
-function myButtonClicked(el:any)
-{
+function validateForm() {
+    if (validaCode() && validaThrusters() && validaPropulsores()) {
+        introduceRocket();
+        return true
+    } else {
+        return false;
+    }
+}
+
+function validaPropulsores() {
+    let potencia: any = document.getElementById("potencia");
+    let numeroPropul: any = document.getElementById("thrusters")
+    var arr: Number[] = potencia.value.split(",").map(Number);
+    let arrPrecessed = validaArraydeNums(arr);
+    console.log("Validacion de propulsores");
+    console.log(arrPrecessed.length);
+    console.log(arrPrecessed.length != parseInt(numeroPropul.value));
+    console.log(potencia.validity.valueMissing);
+    console.log("--------");
+    if (!potencia.checkValidity()) {
+        console.log("loco");
+        if (potencia.validity.valueMissing) {
+            alert("Introduce un valor numérico por número de propulsores separado por comas");
+        }
+        else if (arrPrecessed.length != parseInt(numeroPropul.value)) {
+            console.log("AAAAAA");
+            alert("El número de propulsores no se corresponde con las potencias introducidas");
+        }
+        return false;
+    }
+    return true;
+}
+
+
+function validaArraydeNums(arrPotencia: any[]) {
+    let arrTemp = arrPotencia;
+    for (let index = 0; index < arrPotencia.length; index++) {
+        if (isNaN(arrPotencia[index])) {
+            arrTemp.splice(index, 1);
+        }
+    }
+    // console.log(arrTemp);
+    return arrTemp;
+}
+
+function validaCode() {
+    var elemento: any = document.getElementById("code");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            alert("Introduce un valor de 8 dígitos en el código del cohete");
+        }
+        else if (elemento.validity.patternMismatch) {
+            alert("Estas seguro que tiene 8 dígitos?")
+        }
+        return false;
+    }
+    return true;
+}
+
+function validaThrusters() {
+    var elemento: any = document.getElementById("thrusters");
+    if (!elemento.checkValidity()) {
+        if (elemento.validity.valueMissing) {
+            alert("Introduce un valor numérico en los propulsores");
+        }
+        else if (elemento.validity.patternMismatch) {
+            alert("Estas seguro que es un valor numérico?")
+        }
+        return false;
+    }
+    return true;
+}
+
+
+function myButtonClicked(el: any) {
     el.disabled = true;
-    toggle(); 
+    toggle();
+}
+
+function clearFields() {
+    let codeName: any = document.getElementById("code");
+    let thrusters: any = document.getElementById("thrusters");
+    let potencia: any = document.getElementById("potencia");
+    if (codeName.value != "" || thrusters.value != "" || potencia.value != "") {
+        codeName.value = "";
+        thrusters.value = "";
+        potencia.value = "";
+    }
 }
